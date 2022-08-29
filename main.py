bd7d136b1bcd414ab7399c54170fd7538a446a62
Tran Van Hoang 20173129 <hoang.tv173129@sis.hust.edu.vn>
2022-08-29 22:12:18 +0700

    done model and training code

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import time
+import dgl
+import torch
 import pandas as pd
 import scanpy as sc
 import anndata as ad
@@ -5,46 +8,75 @@ import logging
 import pickle as pk
 from sklearn.model_selection import train_test_split
 from sklearn.decomposition import TruncatedSVD
+from argparse import Namespace
+
+from datetime import datetime
 
 from utils import *
 
 dataset_path = 'data/cite_BMMC_processed/'
-pretrain_path= 'pretrain/cite_BMMC_processed/'
+pretrain_path = 'pretrain/cite_BMMC_processed/'
 
 param = {
     'use_pretrained_data': True,
     'input_train_mod1': f'{dataset_path}mod1.h5ad',
     'input_train_mod2': f'{dataset_path}mod2.h5ad',
-    'n_components_mod1': 100,
-    'n_components_mod2': 100,
-    'random_seed': 17,
     'output_pretrain': 'pretrain',
     'input_train_mod1_pretrained': f'{pretrain_path}mod1_train_svd.pkl',
     'input_train_mod2_pretrained': f'{pretrain_path}mod2_train_svd.pkl',
     'input_test_mod1_pretrained': f'{pretrain_path}mod1_test_svd.pkl',
     'input_test_mod2_pretrained': f'{pretrain_path}mod2_test_svd.pkl',
     'mod1_reducer_pretrained': f'{pretrain_path}svd_mod1.pkl',
-    'mod2_reducer_pretrained': f'{pretrain_path}svd_mod2.pkl'
+    'mod2_reducer_pretrained': f'{pretrain_path}svd_mod2.pkl',
+    'save_model_path': 'saved_model/',
+    'logs_path': 'logs/'
 }
 
+args = Namespace(
+    mod1_feat_size=100,
+    mod2_feat_size=100,
+    out_mod1_feats=100,
+    out_mod2_feats=100,
+    random_seed=17,
+    hid_feats=100,
+    latent_feats=20,
+    num_gcn1_layer=7,  # if using residual, this number must be odd
+    num_gcn2_layer=7,  # if using residual, this number must be odd
+    activation='relu',
+    num_encoder_layer=7,  # if using residual, this number must be odd
+    num_decoder_layer=7,  # if using residual, this number must be odd
+    num_mod1_layer=7,  # if using residual, this number must be odd
+    num_mod2_layer=7,  # if using residual, this number must be odd
+    epochs=1000,
+    lr1=1e-2,
+    lr2=1e-2,
+    k1=10,
+    k2=10,
+    normalization='batch',
+    patience=2
+)
+
+now = datetime.now()
+logger = open(f'{param["logs_path"]}{now.strftime("%d_%m_%Y %H_%M_%S")}.log', 'w')
+logger.write(str(args) + '\n')
+
+logging.info('Reading `h5ad` files...')
+train_mod1 = sc.read_h5ad(param['input_train_mod1'])
+mod1 = train_mod1.var['feature_types'][0]
+train_mod2 = sc.read_h5ad(param['input_train_mod2'])
+mod2 = train_mod2.var['feature_types'][0]
 
 if not param['use_pretrained_data']:
-    logging.info('Reading `h5ad` files...')
-    train_mod1 = sc.read_h5ad(param['input_train_mod1'])
-    mod1 = train_mod1.var['feature_types'][0]
-    dataset_id = train_mod1.uns['dataset_id']
-    input_train_mod1 = train_mod1.X
 
-    train_mod2 = sc.read_h5ad(param['input_train_mod2'])
-    var = train_mod2.var
-    mod2 = train_mod2.var['feature_types'][0]
+    # dataset_id = train_mod1.uns['dataset_id']
+    input_train_mod1 = train_mod1.X
     input_train_mod2 = train_mod2.X
 
     mod1_train, mod1_test, mod2_train, mod2_test = train_test_split(input_train_mod1, input_train_mod2, test_size=0.1,
                                                                     random_state=param['random_seed'])
 
-    mod1_train, mod1_reducer = embedding(mod1_train, param['n_components_mod1'], random_seed=param['random_seed'])
-    mod2_train, mod2_reducer = embedding(mod2_train, param['n_components_mod1'], random_seed=param['random_seed'])
+    mod1_train, mod1_reducer = embedding(mod1_train, args.mod1_feat_size, random_seed=args.random_seed)
+    mod2_train, mod2_reducer = embedding(mod2_train, args.mod2_feat_size, random_seed=args.random_seed)
 
     mod1_test = mod1_reducer.transform(mod1_test)
     mod2_test = mod2_reducer.transform(mod2_test)
@@ -62,3 +94,122 @@ else:
     mod2_test = pk.load(open(param['input_test_mod2_pretrained'], "rb"))
     mod1_reducer = pk.load(open(param['mod1_reducer_pretrained'], "rb"))
     mod2_reducer = pk.load(open(param['mod2_reducer_pretrained'], "rb"))
+
+mod1_train_label = torch.from_numpy(mod1_train)
+mod2_train_label = torch.from_numpy(mod2_train)
+mod1_test_label = torch.from_numpy(mod1_test)
+mod2_test_label = torch.from_numpy(mod2_test)
+
+# graph1 = knn_graph_construction(mod1_train, args.k)
+# graph2 = knn_graph_construction(mod2_train, args.k)
+graph1_test = knn_graph_construction(mod1_test, args.k1)
+graph2_test = knn_graph_construction(mod2_test, args.k2)
+
+net = JointGCNAutoEncoder(args)
+logger.write(str(net) + '\n')
+
+# create optimizer when reconstruct modality 1
+mod1_param = []
+mod1_param.extend(net.gcn1.parameters())
+mod1_param.extend(net.hid_encoder.parameters())
+mod1_param.extend(net.hid_decoder.parameters())
+mod1_param.extend(net.mod1_decoder.parameters())
+opt1 = torch.optim.Adam(mod1_param, args.lr1)
+
+# create optimizer when reconstruct modality 2
+mod2_param = []
+mod2_param.extend(net.gcn2.parameters())
+mod2_param.extend(net.hid_encoder.parameters())
+mod2_param.extend(net.hid_decoder.parameters())
+mod2_param.extend(net.mod2_decoder.parameters())
+opt2 = torch.optim.Adam(mod2_param, args.lr2)
+
+val1_loss = []
+val2_loss = []
+training1_loss = []
+training2_loss = []
+dur = []
+criterion = nn.MSELoss()
+trigger_times1 = 0
+trigger_times2 = 0
+for epoch in range(args.epochs):
+    logger.write(f'epoch:  {epoch}\n')
+
+    net.train()
+
+    # reconstruct modality 1
+    out = net(graph1_test, mod1_test_label, type='x2x', residual=True)
+    loss = criterion(out, mod1_test_label)
+    running_loss = loss.item()
+    opt1.zero_grad()
+    loss.backward()
+    opt1.step()
+    training1_loss.append(running_loss)
+    logger.write(f'training loss 1:  {training1_loss[-1]}\n')
+    logger.flush()
+
+    # reconstruct modality 2
+    out = net(graph2_test, mod2_test_label, type='u2u', residual=True)
+    loss = criterion(out, mod2_test_label)
+    running_loss = loss.item()
+    opt2.zero_grad()
+    loss.backward()
+    opt2.step()
+    training2_loss.append(running_loss)
+    logger.write(f'training loss 2:  {training2_loss[-1]}\n')
+    logger.flush()
+
+    # validate
+    val1 = validate(net, graph1_test, mod1_test_label, type='x2x', residual=True)
+    val1_loss.append(val1)
+    logger.write(f'validation loss 1:  {val1}\n')
+    logger.flush()
+
+    val2 = validate(net, graph2_test, mod2_test_label, type='u2u', residual=True)
+    val2_loss.append(val2)
+    logger.write(f'validation loss 2:  {val2}\n')
+    logger.flush()
+
+    # calculate rmse for mod1 to mod2
+    net.eval()
+    with torch.no_grad():
+        out = net(graph1_test, mod1_test_label, type='x2u', residual=True)
+        out = out.detach().cpu()
+        metric = calculate_rmse(mod2_reducer.inverse_transform(mod2_test_label),
+                                mod2_reducer.inverse_transform(mod1_test_label))
+
+    logger.write(f'rmse {mod1} to {mod2}:  {metric}\n')
+    logger.flush()
+
+    # calculate rmse for mod2 to mod1
+    net.eval()
+    with torch.no_grad():
+        out = net(graph2_test, mod2_test_label, type='u2x', residual=True)
+        out = out.detach().cpu()
+        metric = calculate_rmse(mod1_reducer.inverse_transform(mod1_test_label),
+                                mod1_reducer.inverse_transform(mod2_test_label))
+
+    logger.write(f'rmse {mod2} to {mod1}:  {metric}\n')
+    logger.flush()
+
+    # early stopping
+    if len(training1_loss) > 2 and training1_loss[-1] > training1_loss[-2]:
+        trigger_times1 += 1
+        if trigger_times1 >= args.patience:
+            logger.write(f'early stopping for mod1 trigger\n')
+            logger.flush()
+            break
+    else:
+        trigger_times1 = 0
+
+    if len(training2_loss) > 2 and training2_loss[-1] > training2_loss[-2]:
+        trigger_times2 += 1
+        if trigger_times2 >= args.patience:
+            logger.write(f'early stopping for mod1 trigger\n')
+            logger.flush()
+            break
+    else:
+        trigger_times2 = 0
+
+    print(epoch)
+torch.save(net, f'{param["save_model_path"]}model {mod1} {mod2} {now.strftime("%d_%m_%Y %H_%M_%S")}.pkl')
--- utils.py
+++ utils.py
@@ -1,11 +1,15 @@
+import torch
+import dgl
 import scanpy as sc
-
+import torch.nn.functional as F
 from sklearn.decomposition import TruncatedSVD
+from torch import nn
+from dgl.nn.pytorch.conv import GraphConv
 
 
 def embedding(mod, n_components, random_seed=0):
-    sc.pp.log1p(mod)
-    sc.pp.scale(mod)
+    # sc.pp.log1p(mod)
+    # sc.pp.scale(mod)
 
     mod_reducer = TruncatedSVD(n_components=n_components, random_state=random_seed)
     mod_reducer.fit(mod)
@@ -13,3 +17,318 @@ def embedding(mod, n_components, random_seed=0):
     del mod
     return truncated_mod, mod_reducer
 
+
+def knn_graph_construction(mod_train, k):
+    mod_train = torch.Tensor(mod_train)
+    knn_graph = dgl.knn_graph(mod_train, k)
+    knn_graph = dgl.add_self_loop(knn_graph)
+    return knn_graph
+
+
+def validate(model, graph, labels, type='x2x', residual=True):
+    model.eval()
+    with torch.no_grad():
+        out = model(graph, labels, type, residual)
+        loss = F.mse_loss(out, labels)
+        return loss
+
+
+def calculate_rmse(mod, mod_answer):
+    from sklearn.metrics import mean_squared_error
+    return mean_squared_error(mod, mod_answer, squared=False)
+
+
+class GCNlayer(nn.Module):
+    def __init__(self, feat_size, hid_feats, num_layer, activation):
+        super().__init__()
+        self.num_layer = num_layer
+        self.gcn = nn.ModuleList()
+        self.gcn_acts = nn.ModuleList()
+        self.gcn_norm = nn.ModuleList()
+
+        # create gcn1 layer
+        self.gcn.append(GraphConv(feat_size, hid_feats))
+        for i in range(num_layer - 1):
+            self.gcn.append(GraphConv(hid_feats, hid_feats))
+
+        if activation == 'gelu':
+            for i in range(num_layer):
+                self.gcn_acts.append(nn.GELU())
+        elif activation == 'prelu':
+            for i in range(num_layer):
+                self.gcn_acts.append(nn.PReLU())
+        elif activation == 'relu':
+            for i in range(num_layer):
+                self.gcn_acts.append(nn.ReLU())
+        elif activation == 'leaky_relu':
+            for i in range(num_layer):
+                self.gcn_acts.append(nn.LeakyReLU())
+
+    def forward(self, graph, mod, residual=False):
+
+        if residual:
+            mod = self.gcn[0](graph, mod)
+            mod = self.gcn_acts[0](mod)
+
+            for i in range(1, int((self.num_layer - 1) / 2)):
+                temp = mod
+                mod = self.gcn[i * 2 - 1](graph, mod)
+                mod = self.gcn_acts[i * 2 - 1](mod)
+
+                mod = self.gcn[i * 2](graph, mod) + temp
+                mod = self.gcn_acts[i * 2](mod)
+        else:
+            for i in range(self.num_layer):
+                mod = self.gcn[i](graph, mod)
+                mod = self.gcn_acts[i](mod)
+        return mod
+
+
+class HiddenEncoder(nn.Module):
+    def __init__(self, hid_feats, latent_feats, num_layer, activation, normalization):
+        super().__init__()
+        self.num_layer = num_layer
+        self.hid_encoder = nn.ModuleList()
+        self.encoder_acts = nn.ModuleList()
+        self.encoder_norm = nn.ModuleList()
+
+        # create hidden encoder
+        for i in range(num_layer - 1):
+            self.hid_encoder.append(nn.Linear(hid_feats, hid_feats))
+        self.hid_encoder.append(nn.Linear(hid_feats, latent_feats))
+
+        if activation == 'gelu':
+            for i in range(num_layer):
+                self.encoder_acts.append(nn.GELU())
+        elif activation == 'prelu':
+            for i in range(num_layer):
+                self.encoder_acts.append(nn.PReLU())
+        elif activation == 'relu':
+            for i in range(num_layer):
+                self.encoder_acts.append(nn.ReLU())
+        elif activation == 'leaky_relu':
+            for i in range(num_layer):
+                self.encoder_acts.append(nn.LeakyReLU())
+
+        if normalization == 'batch':
+            for i in range(num_layer - 1):
+                self.encoder_norm.append(nn.BatchNorm1d(hid_feats))
+            self.encoder_norm.append(nn.BatchNorm1d(latent_feats))
+        elif normalization == 'layer':
+            for i in range(num_layer - 1):
+                self.encoder_norm.append(nn.LayerNorm(hid_feats))
+            self.encoder_norm.append(nn.LayerNorm(latent_feats))
+
+    def forward(self, mod, residual=False):
+        if residual:
+            for i in range(int((self.num_layer - 1) / 2)):
+                temp = mod
+                mod = self.hid_encoder[i * 2](mod)
+                mod = self.encoder_acts[i * 2](mod)
+                mod = self.encoder_norm[i * 2](mod)
+
+                mod = self.hid_encoder[i * 2 + 1](mod) + temp
+                mod = self.encoder_acts[i * 2 + 1](mod)
+                mod = self.encoder_norm[i * 2 + 1](mod)
+
+            mod = self.hid_encoder[-1](mod)
+            mod = self.encoder_acts[-1](mod)
+            mod = self.encoder_norm[-1](mod)
+        else:
+            for i in range(self.num_layer):
+                mod = self.hid_encoder[i](mod)
+                mod = self.encoder_acts[i](mod)
+                mod = self.encoder_norm[i](mod)
+        return mod
+
+
+class HiddenDecoder(nn.Module):
+    def __init__(self, hid_feats, latent_feats, num_layer, activation, normalization):
+        super().__init__()
+        self.num_layer = num_layer
+        self.hid_decoder = nn.ModuleList()
+        self.decoder_acts = nn.ModuleList()
+        self.decoder_norm = nn.ModuleList()
+
+        # create hidden decoder
+        self.hid_decoder.append(nn.Linear(latent_feats, hid_feats))
+        for i in range(num_layer - 1):
+            self.hid_decoder.append(nn.Linear(hid_feats, hid_feats))
+
+        if activation == 'gelu':
+            for i in range(num_layer):
+                self.decoder_acts.append(nn.GELU())
+        elif activation == 'prelu':
+            for i in range(num_layer):
+                self.decoder_acts.append(nn.PReLU())
+        elif activation == 'relu':
+            for i in range(num_layer):
+                self.decoder_acts.append(nn.ReLU())
+        elif activation == 'leaky_relu':
+            for i in range(num_layer):
+                self.decoder_acts.append(nn.LeakyReLU())
+
+        if normalization == 'batch':
+            # self.decoder_norm.append(nn.BatchNorm1d(latent_feats))
+            for i in range(num_layer):
+                self.decoder_norm.append(nn.BatchNorm1d(hid_feats))
+        elif normalization == 'layer':
+            # self.decoder_norm.append(nn.LayerNorm(latent_feats))
+            for i in range(num_layer):
+                self.decoder_norm.append(nn.LayerNorm(hid_feats))
+
+    def forward(self, mod, residual=False):
+        if residual:
+            mod = self.hid_decoder[0](mod)
+            mod = self.decoder_acts[0](mod)
+            mod = self.decoder_norm[0](mod)
+
+            for i in range(1, int((self.num_layer - 1) / 2)):
+                temp = mod
+                mod = self.hid_decoder[i * 2 - 1](mod)
+                mod = self.decoder_acts[i * 2 - 1](mod)
+                mod = self.decoder_norm[i * 2 - 1](mod)
+
+                mod = self.hid_decoder[i * 2](mod) + temp
+                mod = self.decoder_acts[i * 2](mod)
+                mod = self.decoder_norm[i * 2](mod)
+        else:
+            for i in range(self.num_layer):
+                mod = self.hid_decoder[i](mod)
+                mod = self.decoder_acts[i](mod)
+                mod = self.decoder_norm[i](mod)
+        return mod
+
+
+class ModalityDecoder(nn.Module):
+    def __init__(self, hid_feats, out_feats, num_layer, activation, normalization):
+        super().__init__()
+        self.num_layer = num_layer
+        self.mod_decoder = nn.ModuleList()
+        self.mod_acts = nn.ModuleList()
+        self.mod_norm = nn.ModuleList()
+
+        # create modality decoder
+        for i in range(num_layer - 1):
+            self.mod_decoder.append(nn.Linear(hid_feats, hid_feats))
+        self.mod_decoder.append(nn.Linear(hid_feats, out_feats))
+
+        if activation == 'gelu':
+            for i in range(num_layer):
+                self.mod_acts.append(nn.GELU())
+        elif activation == 'prelu':
+            for i in range(num_layer):
+                self.mod_acts.append(nn.PReLU())
+        elif activation == 'relu':
+            for i in range(num_layer):
+                self.mod_acts.append(nn.ReLU())
+        elif activation == 'leaky_relu':
+            for i in range(num_layer):
+                self.mod_acts.append(nn.LeakyReLU())
+
+        if normalization == 'batch':
+            for i in range(num_layer - 1):
+                self.mod_norm.append(nn.BatchNorm1d(hid_feats))
+            self.mod_norm.append(nn.BatchNorm1d(out_feats))
+        elif normalization == 'layer':
+            for i in range(num_layer - 1):
+                self.mod_norm.append(nn.LayerNorm(hid_feats))
+            self.mod_norm.append(nn.LayerNorm(out_feats))
+
+    def forward(self, mod, residual=False):
+        if residual:
+            for i in range(int((self.num_layer - 1) / 2)):
+                temp = mod
+                mod = self.mod_decoder[i * 2](mod)
+                mod = self.mod_acts[i * 2](mod)
+                mod = self.mod_norm[i * 2](mod)
+
+                mod = self.mod_decoder[i * 2 + 1](mod) + temp
+                mod = self.mod_acts[i * 2 + 1](mod)
+                mod = self.mod_norm[i * 2 + 1](mod)
+
+            mod = self.mod_decoder[-1](mod)
+            mod = self.mod_acts[-1](mod)
+        else:
+            for i in range(self.num_layer - 1):
+                mod = self.mod_decoder[i](mod)
+                mod = self.mod_acts[i](mod)
+                mod = self.mod_norm[i](mod)
+
+            mod = self.mod_decoder[-1](mod)
+            mod = self.mod_acts[-1](mod)
+        return mod
+
+
+class JointGCNAutoEncoder(nn.Module):
+    def __init__(self, args):
+        super().__init__()
+        self.args = args
+        # define layer
+        self.gcn1 = GCNlayer(args.mod1_feat_size, args.hid_feats, args.num_gcn1_layer, args.activation)
+        self.gcn2 = GCNlayer(args.mod2_feat_size, args.hid_feats, args.num_gcn2_layer, args.activation)
+
+        self.hid_encoder = HiddenEncoder(args.hid_feats, args.latent_feats, args.num_encoder_layer, args.activation,
+                                         args.normalization)
+        self.hid_decoder = HiddenDecoder(args.hid_feats, args.latent_feats, args.num_decoder_layer, args.activation,
+                                         args.normalization)
+
+        self.mod1_decoder = ModalityDecoder(args.hid_feats, args.out_mod1_feats, args.num_mod1_layer, args.activation,
+                                            args.normalization)
+        self.mod2_decoder = ModalityDecoder(args.hid_feats, args.out_mod2_feats, args.num_mod2_layer, args.activation,
+                                            args.normalization)
+
+    def forward(self, graph, mod, type='x2u', residual=False):
+        """
+        :param self:
+        :param graph:
+            graph with data store in graph.ndata['feat']
+        :param type:
+            x2u = mod1 to mod2
+            u2x = mod2 to mod1
+            x2x = mod1 to mod1
+            u2u = mod2 to mod2
+        :param residual:
+            using residual connection or not
+        :return: reconstructed modality
+        """
+
+        if type == 'x2u':
+            mod = self.gcn1(graph, mod, residual)
+            mod = self.hid_encoder(mod, residual)
+            mod = self.hid_decoder(mod, residual)
+            mod = self.mod2_decoder(mod, residual)
+        elif type == 'u2x':
+            mod = self.gcn2(graph, mod, residual)
+            mod = self.hid_encoder(mod, residual)
+            mod = self.hid_decoder(mod, residual)
+            mod = self.mod1_decoder(mod, residual)
+        elif type == 'x2x':
+            mod = self.gcn1(graph, mod, residual)
+            mod = self.hid_encoder(mod, residual)
+            mod = self.hid_decoder(mod, residual)
+            mod = self.mod1_decoder(mod, residual)
+        elif type == 'u2u':
+            mod = self.gcn2(graph, mod, residual)
+            mod = self.hid_encoder(mod, residual)
+            mod = self.hid_decoder(mod, residual)
+            mod = self.mod2_decoder(mod, residual)
+
+        return mod
+
+
+def save_checkpoint(model, optimizer, save_path, epoch):
+    torch.save({
+        'model_state_dict': model.state_dict(),
+        'optimizer_state_dict': optimizer.state_dict(),
+        'epoch': epoch
+    }, save_path)
+
+
+def load_checkpoint(model, optimizer, load_path):
+    checkpoint = torch.load(load_path)
+    model.load_state_dict(checkpoint['model_state_dict'])
+    optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+    epoch = checkpoint['epoch']
+
+    return model, optimizer, epoch
